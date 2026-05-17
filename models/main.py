3bee83856134637fca5409c381f7826ec7274e4e
Javier-MR-Leon <javiermrviento@gmail.com>
2026-05-17 18:11:33 +0200

    Update main.py

--- models/main.py
+++ models/main.py
@@ -39,8 +39,15 @@ def main():
     df_res_clf, df_res_reg = benchmarker.ejecutar_benchmarking(datasets)
     bm.guardar_reportes_visuales(df_clf, df_reg)
 
+    # Screening Mediación
+    df_mediacion_crisis = benchmarker.screening_mediacion_crisis_total_tfm(dict_datasets=datasets, x_var='crisis_encefalop_ticas_v2')
+    display(df_mediacion_crisis.head(20))
+    
+    df_mediacion_bioq = benchmarker.screening_mediacion_bioq_total_tfm(dict_datasets=datasets, x_var='bioquimico')
+    display(df_mediacion_bioq.head(20))
+
     # EVALUACIÓN Y BIOMARCADORES 
-    print("\n Auditando modelos y extrayendo biomarcadores...")
+    print("\n Auditando modelos y extrayendo relaciones...")
     # Genera los reportes finales REPORTE_FINAL_... en 4_GRAPHICS
     df_maestra_clf, df_maestra_reg = evaluator.generar_reporte_final(
         df_res_clf, df_res_reg, datasets, umbral_overfit=15.0
@@ -54,8 +61,17 @@ def main():
     if not df_maestra_reg.empty:
         print(df_maestra_reg[['Dominio', 'Modalidad', 'MAE (Z)', 'Modelo']].to_string(index=False))
 
+    # Se pintan los cerebros 3D y subcorticales en lote
+    evaluador.ejecutar_renderizado_lote_neuroimagen(df_clf_fin, df_reg_fin)
+
+    print(f"Reporte top mediadores para Bioquímico")
+    evaluador.generar_reporte_top_mediadores_v7(df_mega_res_bioq, datasets_listos, x_var='bioquimico', p_umbral=0.15)
+
+    print(f"\nReporte top mediadores para Crisis")
+    evaluador.generar_reporte_top_mediadores_v7(df_mega_res_crisis, datasets_listos, x_var='crisis_encefalop_ticas_v2', p_umbral=0.15)
+
     print("\n==================================================")
-    print("PROCESO DE ML FINALIZADO")
+    print("PROCESO DE ML Y MEDIACIÓN FINALIZADO")
     print("==================================================")
 
 if __name__ == "__main__":
